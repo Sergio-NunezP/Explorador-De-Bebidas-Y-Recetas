c0204155e7f4547e40c85571fae02039440708d1
Sergio Núñez <sergioop722@gmail.com>
2025-03-04 09:37:14 -0500

    useLocation y se crea el formulario de búsqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+
+    const { pathname } = useLocation()
+
+    const isHome = useMemo(() => pathname === '/', [pathname])
+
     return (
         <header className="bg-slate-800">
             <div className="mx-auto container px-5 py-16">
@@ -24,6 +30,45 @@ export default function Header() {
                         >favoritos</NavLink>
                     </nav>
                 </div>
+
+                {isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6" >
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingrediente</label>
+
+                            <input
+                                type="text"
+                                name="ingredient"
+                                id="ingredient"
+                                className="bg-white p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Nombre o Ingrediente: Ej. Vodka, Café"
+                            />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoria</label>
+
+                            <select
+                                name="ingredient"
+                                id="ingredient"
+                                className="bg-white p-3 w-full rounded-lg focus:outline-none"
+                            >
+                                <option value="">-- Seleccione --</option>
+                            </select>
+                        </div>
+                        <input
+                            type="submit"
+                            value="Buscar Recetas"
+                            className="bg-orange-800 hover:bg-orange-900  cursor-pointer font-extrabold text-white w-full p-2 rounded-2xl uppercase"
+                        />
+                    </form>
+                )}
             </div>
         </header>
     )
