fbc884cb603c17f70648e78f1709542d5be2d027
Sergio Núñez <sergioop722@gmail.com>
2025-03-07 10:04:37 -0500

    se llama el slice de notificación para mostrarla

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,5 +1,7 @@
 import { StateCreator } from 'zustand'
 import { Recipe } from '../types'
+import { createNotificationSlice, NotificationSliceType } from './notificationSlice'
+import { RecipesSliceType } from './recipeSlice'
 
 export type FavoritesSliceType = {
     favorites: Recipe[]
@@ -9,23 +11,36 @@ export type FavoritesSliceType = {
 
 }
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipesSliceType & NotificationSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
     favorites: [],
     handleClickFavorite: (recipe) => {
         if (get().favoriteExist(recipe.idDrink)) {
+            //Eliminar una receta
             set((state) => ({
                 favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
             }))
+            // Mostrar notificacion 
+            createNotificationSlice(set, get, api).showNotification({
+                text: 'Se eliminó de favoritos',
+                error: false
+            })
         } else {
+            //Agregar receta
             set((state) => ({
                 favorites: [...state.favorites, recipe]
             }))
+            // Mostrar notificacion 
+            createNotificationSlice(set, get, api).showNotification({
+                text: 'Se agregó a favoritos',
+                error: false
+            })
         }
         localStorage.setItem('favorites', JSON.stringify(get().favorites))
     },
     favoriteExist: (id) => {
         return get().favorites.some(favorite => favorite.idDrink === id)
     },
+    //Almacenar en el localStorgae
     loadFromStorge: () => {
         const storedFavorites = localStorage.getItem('favorites')
         if (storedFavorites) {
